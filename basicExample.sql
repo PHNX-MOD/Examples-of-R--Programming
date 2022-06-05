c65008ecb3551b120ccb5c37913d53f80cd26b93
PHNX-MOD <40522163+PHNX-MOD@users.noreply.github.com>
2022-06-05 14:19:37 -0700

    Update and rename examples.sql to basicExample.sql

--- basicExample.sql
+++ basicExample.sql
@@ -10,7 +10,29 @@ FROM films
 WHERE language  = 'Spanish'
 AND release_year < '2000'
 
+--AND OR
 SELECT title
 FROM films
 WHERE (release_year = 1994 OR release_year = 1995)
 AND (certification = 'PG' OR certification = 'R');
+
+--inclusive
+SELECT title
+FROM films
+WHERE release_year
+BETWEEN 1994 AND 2000;
+
+--OR
+SELECT name
+FROM kids
+WHERE age IN (2, 4, 6, 8, 10);
+
+--is null, not null
+SELECT name
+FROM people
+WHERE birthdate IS NOT NULL;
+
+--like where 
+SELECT name
+FROM people
+WHERE name LIKE 'B%';
